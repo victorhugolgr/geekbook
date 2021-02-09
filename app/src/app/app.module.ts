9d3484b6e96a9dec3f2c022db4361dea74eb913f
victor.hugo <victor.hugo@tre-pa.jus.br>
2021-02-09 08:11:20 -0300

    cabecalho e rodape

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeModule } from './componentes/rodape/rodape.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RodapeModule,
+    CabecalhoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- app/src/app/autenticacao/autenticacao.service.ts
+++ app/src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
+  constructor(private httpClient: HttpClient, private ususarioService: UsuarioService) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  autenticar(usuario: string, senha: string): Observable<any>{
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient.post(
+      'http://localhost:3000/user/login',
+      {
+        userName: usuario,
+        password: senha
+      },
+      { observe: 'response' }
+    ).pipe(
+      tap((res)=>{
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.ususarioService.salvaToken(authToken);
+      })
+    );
   }
 }
